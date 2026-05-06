b2c4e695de7f4f949b46ae2a5f6bb230127a1c99
nullbytekid <marlontk1221+1@gmail.com>
2026-05-06 14:41:58 +0800

    fixing errors

--- libraries/nestjs-libraries/src/integrations/social/x.provider.ts
+++ libraries/nestjs-libraries/src/integrations/social/x.provider.ts
@@ -152,7 +152,7 @@ export class XProvider extends SocialAbstract implements SocialProvider {
     disabled: !!process.env.DISABLE_X_ANALYTICS,
     description:
       'When a post reached a certain number of likes, repost it to increase engagement (1 week old posts)',
-    runEveryMilliseconds: 18000000, // 5 hours
+    runEveryMilliseconds: 300000, // 5 minutes
     totalRuns: 3,
     fields: [
       {
@@ -231,8 +231,8 @@ export class XProvider extends SocialAbstract implements SocialProvider {
     disabled: !!process.env.DISABLE_X_ANALYTICS,
     description:
       'When a post reaches a certain number of likes, automatically reply to the original post with your promotional message.',
-    runEveryMilliseconds: 18000000, // 5 hours
-    totalRuns: 40,
+    runEveryMilliseconds: 300000, // 5 minutes
+    totalRuns: 10,
     fields: [
       {
         name: 'likesAmount',
@@ -289,7 +289,7 @@ export class XProvider extends SocialAbstract implements SocialProvider {
     disabled: !!process.env.DISABLE_X_ANALYTICS,
     description:
       'When a post reaches a certain number of likes, send a Direct Message to those who liked it. Note: Users must follow you or have open DMs, and X API rate limits apply.',
-    runEveryMilliseconds: 18000000, // 5 hours
+    runEveryMilliseconds: 300000, // 5 minutes
     totalRuns: 3,
     fields: [
       {
